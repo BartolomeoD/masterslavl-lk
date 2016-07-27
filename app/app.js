05d9f35e196e84c9c047adccd3ad8c2f8a87e288
white-beard <maximsen@mail.ru>
2016-07-27 13:46:27 +0300

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -15,14 +15,34 @@ angular.module('MS', [
 ])
 .config(function($routeProvider) {
 })
-.controller('MainCtrl', function($scope) {
+.controller('MainCtrl', function($scope, browser) {
    $scope.Hello = "Hello, Bitch"; 
+   $scope.ios=browser();
 })
-.run( function($rootScope, $location, $cookies) {
+.run( function($rootScope, $location, $cookies, browser) {
     if($cookies.get("auth")){
 		$location.path('app/home');
+		console.log(browser());
 	}
 	else{
 		 document.location.href='index.html';		
 	}
-});
+})
+.service('browser', ['$window', function($window) {
+
+     return function() {
+
+         var userAgent = $window.navigator.userAgent;
+
+        var browsers = {chrome: /chrome/i, safari: /safari/i, firefox: /firefox/i, ie: /internet explorer/i};
+
+        for(var key in browsers) {
+            if (browsers[key].test(userAgent)) {
+                return key;
+            }
+       };
+
+       return 'unknown';
+    }
+
+}]);
